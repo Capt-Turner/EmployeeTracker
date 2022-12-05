9e1d07928a305433a4375ff3696644fb533dbf3c
Capt-Turner <sethhet@gmail.com>
2022-12-04 23:24:07 -0700

    added .env to hold sensitive variables+defined DB schema

--- db/connection.js
+++ db/connection.js
@@ -1,1 +1,17 @@
 const mysql=require('mysql2');
+const user=process.env.USER;
+const pass=process.env.PW;
+const db=process.env.DB
+
+const connection=mysql.createConnection({
+    host: "localhost",
+    user: user,
+    password: pass,
+    database: db
+});
+
+connection.connect(function(err){
+    if(err) throw err;
+});
+
+module.exports=connection;
--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,29 @@ DROP DATABASE IF EXISTS employees;
 CREATE DATABASE employees;
 
 USE employees;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE role (
+    id INTO AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    INDEX dep_ind (department_id),
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES deparment(id)
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    INDEX role_ind (role_id),
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id),
+    manager_id INT,
+    INDEX man_id (manager_id),
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
