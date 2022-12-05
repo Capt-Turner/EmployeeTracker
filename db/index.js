17d942b6ab6c4b2b6356e2521f22cd63def92155
Capt-Turner <sethhet@gmail.com>
2022-12-04 23:35:23 -0700

    added queries to find departments, roles, and employees

--- db/index.js
+++ db/index.js
@@ -5,4 +5,22 @@ class DB{
         this.connection=connection;
     };
 
+    findAllDepartments(){
+        return this.connection.promise().query(
+            "SELECT department.id, department.name FROM department;"
+        )
+    };
+
+    findAllRoles(){
+        return this.connection.promise().query(
+            "SELECT role.id, role.title,department.name AS department;"
+        )
+    };
+
+    findAllEmployees(){
+        return this.connection.promise().query(
+            "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary;"
+        )      
+    };
+
 }
